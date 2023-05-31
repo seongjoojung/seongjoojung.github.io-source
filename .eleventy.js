a80a086ca07a7e9f37487ed4afdf9780e6297d99
seongjoojung <seongjoojung@gmail.com>
2023-05-31 18:33:13 -0500

    initial

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+const { DateTime } = require('luxon');
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/images/");
+    eleventyConfig.addWatchTarget("./src/images/");
+    eleventyConfig.addPassthroughCopy("./src/pdfs/");
+    eleventyConfig.addWatchTarget("./src/pdfs/");
+
+    eleventyConfig.addFilter('readableDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+          'DDD'
+        );
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "docs"
+        }
+    };
+};
---
+++ src/posts/first-post.md
@@ -0,0 +1,11 @@
+---
+title: First Post
+description: This is the first post of the blog, which I hope to keep up and use it to communicate.
+tags: post
+date: 2023-05-31
+layout: layouts/post.njk
+---
+
+This is the first post of the blog, which I hope to keep up and use it to communicate.
+
+ The blog was made with <a href="https://www.11ty.dev/">11ty</a> and helpful guide from <a href=https://www.digitalocean.com/community/tutorials/how-to-create-and-deploy-your-first-eleventy-website>digital ocean</a>
